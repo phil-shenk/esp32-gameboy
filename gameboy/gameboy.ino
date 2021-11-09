7b930fe7e42c6aef48105b25f68769588c469735
phil-shenk <shenk.philip@gmail.com>
2021-11-08 23:14:47 -0500

    removed duplicate files and added Serial output

--- gameboy/gameboy.ino
+++ gameboy/gameboy.ino
@@ -9,18 +9,22 @@
 
 
 void setup() {
+  
+  Serial.println("welcome to esp32 gameboy with no display.\n");
+  
   // put your setup code here, to run once:
   int r = rom_init(gb_rom);
-
   sdl_init();
-
   printf("ROM OK!\n");
+  Serial.println("ROM OK!\n");
 
   gb_mem_init();
   printf("Mem OK!\n");
+  Serial.println("GB MEM OK!\n");
 
   cpu_init();
   printf("CPU OK!\n");
+  Serial.println("CPU INIT OK!\n");
 }
 
 void loop() {
